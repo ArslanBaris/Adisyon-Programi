4ae998deec9c20f8a03b081629306ee9bc534138
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-05-23 15:48:28 +0300

    feat: Service added.

---
+++ app/src/main/java/cbu/httf/adisyonprogram/Network/Service.java
@@ -0,0 +1,36 @@
+package cbu.httf.adisyonprogram.Network;
+
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import cbu.httf.adisyonprogram.data.model.Cons;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class Service {
+
+    public static ServiceApi serviceApi;
+    //public static Gson gson = new GsonBuilder().setLenient().create();
+
+    public  static Retrofit retrofit;
+
+    private static Retrofit getInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Cons.BASE_URL)
+                   // .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static ServiceApi getServiceApi() {
+
+        serviceApi = getInstance().create(ServiceApi.class);
+        return serviceApi;
+    }
+}
