b3bb2d4c1d48e57980f0be36f70239071c51e9d2
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-05-28 18:55:34 +0300

    refactor: CategoryAdapter revised.

--- app/src/main/java/cbu/httf/adisyonprogram/Adapters/CategoryAdapter.java
+++ app/src/main/java/cbu/httf/adisyonprogram/Adapters/CategoryAdapter.java
@@ -1,9 +1,11 @@
 package cbu.httf.adisyonprogram.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,11 +20,13 @@ import cbu.httf.adisyonprogram.data.model.TablesModel;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoriesHolder> {
 
+
+
     private Context mContext;
     private ArrayList<CategoryModel> mCategoryList;
     private OnCategoryItemClickListener listener;
 
-
+    private int selected_position=-1;
     public CategoryAdapter(ArrayList<CategoryModel> mCategoryList, Context mContext) {
         this.mCategoryList=mCategoryList;
         this.mContext=mContext;
@@ -36,9 +40,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CategoriesHolder holder, int position) {
+    public void onBindViewHolder(@NonNull  CategoriesHolder holder, int position) {
         holder.txtMenuCategory.setText(String.valueOf(mCategoryList.get(position).getCategoryName()));
 
+
+        if (selected_position == position) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#A1A1A1"));
+        }else{
+            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
     }
 
     @Override
@@ -49,20 +59,24 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
 
     public class CategoriesHolder extends RecyclerView.ViewHolder{
         TextView txtMenuCategory;
-
+        LinearLayout linearLayout;
 
         public CategoriesHolder(@NonNull View itemView) {
             super(itemView);
             txtMenuCategory=itemView.findViewById(R.id.category_item_txtCategory);
-
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int position = getAdapterPosition();
 
-                    if(listener!= null && position!=RecyclerView.NO_POSITION){
-                        listener.onCategoryItemClick(mCategoryList.get(position),position);
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onCategoryItemClick(mCategoryList.get(position),position);
+                            selected_position = position;
+                            notifyDataSetChanged();
+                        }
                     }
+
                 }
             });
         }
