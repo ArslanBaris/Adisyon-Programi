abc9c663a00f69a1d5b748611c46e82d84d0eb42
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-05-30 02:55:07 +0300

    refactor: LoginResponse revised.

--- app/src/main/java/cbu/httf/adisyonprogram/data/model/LoginResponse.java
+++ app/src/main/java/cbu/httf/adisyonprogram/data/model/LoginResponse.java
@@ -1,14 +1,5 @@
 package cbu.httf.adisyonprogram.data.model;
 
-import android.util.Base64;
-import android.util.Log;
-
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class LoginResponse {
